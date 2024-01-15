8f19d4e0e73682967ca2509ff3488c15442f5ace
“Jacob <jacov141@gmail.com>
2024-01-15 18:22:28 +0200

    create app

--- graviton_workshop_create_ddb_cdk/graviton_workshop_create_ddb_cdk_stack.py
+++ graviton_workshop_create_ddb_cdk/graviton_workshop_create_ddb_cdk_stack.py
@@ -1,19 +1,23 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
 
+import aws_cdk as cdk
+import aws_cdk.aws_dynamodb as dynamodb
+from constructs import Construct
+from aws_cdk import Stack
 class GravitonWorkshopCreateDdbCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # create a dynamodb table
+        urls_table = dynamodb.Table(
+            self, "GravitonWorkshopDdbUrlsTable",
+            partition_key=dynamodb.Attribute(
+                name="url",
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "GravitonWorkshopCreateDdbCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        account = self.account
+        print(f"This stack is being deployed to account: {account}")
+       
